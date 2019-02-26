ae357ca1d7821508146e31a8a290756c94a48a82
Vadim Holubnychyi <v.holubnychyi@aimprosoft.com>
2019-02-26 15:54:42 +0200

    refactoring

--- src/main/scala/generator/App.scala
+++ src/main/scala/generator/App.scala
@@ -12,8 +12,6 @@ object App {
     * @param args parameters used to launch this application
     */
   def main(args: Array[String]): Unit = {
-    val domain = java.net.InetAddress.getLocalHost.getHostName
-
     if (args(0) == "generate-key") {
       println(Encoder.encode(args(1), Util.parseDateToString(args(2))))
     } else if (args(0) == "validate-key") {
