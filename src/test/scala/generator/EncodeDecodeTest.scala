2d7a7760277ea94b994ada84915e2285d1cdbd34
Igor <ihor.makivskiy@gmail.com>
2019-02-26 16:14:12 +0200

    Add tests

---
+++ src/test/scala/generator/EncodeDecodeTest.scala
@@ -0,0 +1,48 @@
+package generator
+
+import java.time.LocalDate
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class EncodeDecodeTest extends FlatSpec with Matchers {
+  "Decoder method isExpired" should "check date in key for expiration and return true" in {
+    val date = LocalDate.of(2020, 11, 11)
+
+    val key = Encoder.encode("domain.com", date).split("-").mkString
+
+    Decoder.isExpired(key) shouldBe true
+  }
+
+  "Decoder method isExpired" should "check date in key for expiration and return false" in {
+    val date = LocalDate.of(2000, 11, 11)
+
+    val key = Encoder.encode("domain.com", date).split("-").mkString
+
+    Decoder.isExpired(key) shouldBe false
+  }
+
+  "Decoder method verify" should "return false when date is invalid" in {
+    val date = LocalDate.of(2000, 11, 11)
+
+    val key = Encoder.encode("domain.com", date).split("-").mkString
+
+    Decoder.verify(key, "domain.com") shouldBe false
+  }
+
+  "Decoder method verify" should "return false when domain is invalid" in {
+    val date = LocalDate.of(2000, 11, 11)
+
+    val key = Encoder.encode("domain.com", date)
+
+    Decoder.verify(key, "domain.co") shouldBe false
+  }
+
+  "Decoder method verify" should "return true when all data is fine" in {
+    val date = LocalDate.of(2020, 11, 11)
+
+    val key = Encoder.encode("domain.com", date)
+
+    Decoder.verify(key, "domain.com") shouldBe true
+  }
+
+}
