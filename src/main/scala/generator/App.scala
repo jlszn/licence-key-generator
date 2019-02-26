adb6083f0c7bee5aca8f1c1edcf039a0da596904
Vadim Holubnychyi <v.holubnychyi@aimprosoft.com>
2019-02-26 16:36:00 +0200

    Refactoring

--- src/main/scala/generator/App.scala
+++ src/main/scala/generator/App.scala
@@ -12,12 +12,17 @@ object App {
     * @param args parameters used to launch this application
     */
   def main(args: Array[String]): Unit = {
-    if (args(0) == Messages.generateKeyCommand) {
-      println(Encoder.encode(args(1), Util.parseDateToString(args(2))))
-    } else if (args(0) == Messages.validateKeyCommand) {
-      Decoder.verify(args(1), args(2))
-    } else {
-      println(Messages.unknownCommand)
+
+    args(0) match {
+
+      case Messages.generateKeyCommand => println(Encoder.encode(args(1), Util.parseDateToString(args(2))))
+
+      case Messages.validateKeyCommand => Decoder.verify(args(1), args(2))
+
+      case Messages.infoCommand => println(Messages.info)
+
+      case _ => println(Messages.unknownCommand)
+
     }
   }
 
--- src/main/scala/generator/utils/Messages.scala
+++ src/main/scala/generator/utils/Messages.scala
@@ -20,4 +20,15 @@ object Messages {
 
   val hashingAlgorithm = "SHA-512"
 
+  val infoCommand = "info"
+
+  val info =
+    s"""
+      |This application can be used for a key generation and validation.
+      |It has 3 options:
+      | 1) Generate key - Run as $generateKeyCommand <domain> <date>. Date has dd-mm-yyyy format
+      | 2) Validate key - Run as $validateKeyCommand <key> <domain>
+      | 3) Info - Run as $infoCommand
+    """.stripMargin
+
 }
